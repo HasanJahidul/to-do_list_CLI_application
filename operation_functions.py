cac281fd0268465257d4287b2eeaa7111b31efc1
hasanjahidul <jahidul0hasan@gmail.com>
2022-06-18 19:37:33 +0600

    Add function added

--- operation_functions.py
+++ operation_functions.py
@@ -1,23 +1,33 @@
 from printing_functions import thanks
-todo_list=[]
+todo_list = []
 
-#handle the operation of the program
+# handle the operation of the program
+def add_task():
+    task = input("Enter a new task: ")
+    todo_list.append(["", "", "", ""])
+    todo_list[-1][0] = task
+    todo_list[-1][1] = input("Enter the date: ")
+    todo_list[-1][2] = input("Enter the time: ")
+    todo_list[-1][3] = input("Enter the status: ")
+
+def update_task():
+    task_or_date = input("Enter a task or date: ")
+    # search in list and print index
+    for index, task in enumerate(todo_list):
+        if task[0] == task_or_date or task[1] == task_or_date:
+            #update the task
+            todo_list[index][0] = input("Enter the Updated task: ")
+            todo_list[index][1] = input("Enter the Updated date: ")
+            todo_list[index][2] = input("Enter the Updated time: ")
+            todo_list[index][3] = input("Enter the Updated status: ")
+
+def delete_task():
+    task_or_date = input("Enter a task or date: ")
+    todo_list.pop()
+
 def operation(operation_type, operation_value, operation_list):
-    if operation_type == "add":
-        #add new task with date, time and status
-        operation_list.append(["", "", "", ""])
-        operation_list[-1][0]=operation_value
-        operation_list[-1][1]=input("Enter the date: ")
-        operation_list[-1][2]=input("Enter the time: ")
-        operation_list[-1][3]=input("Enter the status: ")
-    elif operation_type == "update":
-        #update task with date, time and status
-        operation_list[operation_value[0]][0]=operation_value[1]
-        operation_list[operation_value[0]][1]=input("Enter the date: ")
-        operation_list[operation_value[0]][2]=input("Enter the time: ")
-        operation_list[operation_value[0]][3]=input("Enter the status: ")
-    elif operation_type == "delete":
-        #delete task
+    if operation_type == "delete":
+        # delete task
         operation_list.pop(operation_value)
     elif operation_type == "view":
         print(operation_list)
@@ -36,26 +46,24 @@ def operation(operation_type, operation_value, operation_list):
 
 
 def user_input():
-    menu=input("Enter your choice: ")
+    menu = input("Enter your choice: ")
     while menu != "7":
         if menu == "1":
-            task=input("Enter a new task: ")
-            operation("add", task, todo_list)
+            add_task()
         elif menu == "2":
-            task_index=int(input("Enter the index of the task you want to update: "))
-            task=input("Enter the new task: ")
-            operation("update", [task_index, task], todo_list)
+           update_task()
         elif menu == "3":
-            task_index=int(input("Enter the index of the task you want to delete: "))
+            task_index = int(
+                input("Enter the index of the task you want to delete: "))
             operation("delete", task_index, todo_list)
         elif menu == "4":
             operation("view", None, todo_list)
         elif menu == "5":
             operation("save", None, todo_list)
         elif menu == "6":
-            task=input("Enter the task you want to search: ")
+            task = input("Enter the task you want to search: ")
             operation("search", task, todo_list)
         else:
             print("Invalid choice")
-        menu=input("Enter your choice: ")
+        menu = input("Enter your choice: ")
     thanks()
